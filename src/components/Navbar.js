da94dbb6d6d46ad5dde35230a53a18607aa435c5
mac <mac@Darpans-MacBook-Pro.local>
2026-04-01 16:12:16 +0530

    removed resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,27 +81,6 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            {/* <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item> */}
-
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/idarpan-sharma"
